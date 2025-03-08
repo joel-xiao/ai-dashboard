950045329dbecec43cd3726123c5aacfebf9c280
肖文龙 <joel.xiao@dclingcloud.com>
2025-03-08 23:04:39 +0800

    dashboard 提示词

--- template-1/db-1.js
+++ template-1/db-1.js
@@ -22,7 +22,7 @@ const layout_nodes = nodes.map((node) => {
 });
 
 const new_layout_nodes = layout_nodes.splice(0, 6);
-console.log('这是我的 nodes JSON',  '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
+console.log('这是我的 nodes JSON', '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
 console.log(JSON.stringify(new_layout_nodes))
 
 const charts = {};
@@ -33,33 +33,33 @@ nodes.forEach(node => {
   if (prev_n?.properties.databind?.leng) return;
 
   let n = node;
-  if(n.properties) {
-      n.properties = JSON.parse(n.properties);
+  if (n.properties) {
+    n.properties = JSON.parse(n.properties);
   }
 
-    delete n.z;
-    delete n.x;
-    delete n.y;
-    delete n.w;
-    delete n.h;
-    delete n.name;
-    delete n.isHide;
-    delete n.isLock;
-    delete n.dashboardId;
-    delete n.id;
-    delete n.ver;
+  delete n.z;
+  delete n.x;
+  delete n.y;
+  delete n.w;
+  delete n.h;
+  delete n.name;
+  delete n.isHide;
+  delete n.isLock;
+  delete n.dashboardId;
+  delete n.id;
+  delete n.ver;
 
-    charts[node.type] = node;
+  charts[node.type] = node;
 });
 
 
 console.log('这是我的 数据源 和 数据绑定 JSON', '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>');
 Object.values(charts).forEach(n => {
-  console.log( 'chart databind and dataSource ======================================================================================================================== start')
-  console.log( 'type=' + n.type, 'node.properties.databind', JSON.stringify(n.properties.databind))
+  console.log('chart databind and dataSource ======================================================================================================================== start')
+  console.log('type=' + n.type, 'node.properties.databind', JSON.stringify(n.properties.databind))
   console.log('-----------------------------------------------------------------------------------------------------------------------------------------------------------')
-  console.log( 'type=' + n.type, 'node.properties.dataSource', JSON.stringify(n.properties.dataSource))
-  console.log( 'chart databind and dataSource ======================================================================================================================== end')
+  console.log('type=' + n.type, 'node.properties.dataSource', JSON.stringify(n.properties.dataSource))
+  console.log('chart databind and dataSource ======================================================================================================================== end')
 });
 
 // 这是我的图标类型
@@ -67,9 +67,9 @@ console.log('这是我的图表类型 JSON', '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 Object.values(charts).forEach(n => {
   n.properties.databind = [];
   n.properties.dataSource = [];
-  console.log( 'chart JSON ======================================================================================================================== start')
-  console.log( 'type=' + n.type, JSON.stringify(n))
-  console.log( 'chart JSON ======================================================================================================================== end')
+  console.log('chart JSON ======================================================================================================================== start')
+  console.log('type=' + n.type, JSON.stringify(n))
+  console.log('chart JSON ======================================================================================================================== end')
 });
 
 // console.log('node Charts', JSON.stringify(charts))
